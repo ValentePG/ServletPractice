d81cda28e2861dcd99708e885be136e2c88dec65
Gabriel Valente <112831085+ValentePG@users.noreply.github.com>
2024-11-06 15:40:56 -0300

    inicio

---
+++ src/main/java/dev/valente/model/UsuarioTeste.java
@@ -0,0 +1,31 @@
+package dev.valente.model;
+
+public class UsuarioTeste {
+    private String name;
+    private String role;
+    private String profission;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getProfission() {
+        return profission;
+    }
+
+    public void setProfission(String profission) {
+        this.profission = profission;
+    }
+}
---
+++ src/main/java/dev/valente/servelet/HelloServlet.java
@@ -0,0 +1,33 @@
+package dev.valente.servelet;
+
+import com.google.gson.Gson;
+import dev.valente.model.UsuarioTeste;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        PrintWriter printWriter = resp.getWriter();
+
+        resp.setContentType("text/json");
+        UsuarioTeste user1 = new UsuarioTeste();
+
+        user1.setName("GABRIEL");
+        user1.setRole("ADMIN");
+        user1.setProfission("DEV");
+
+        Gson gson = new Gson();
+        String response = gson.toJson(user1);
+
+        printWriter.println(response);
+    }
+
+}
