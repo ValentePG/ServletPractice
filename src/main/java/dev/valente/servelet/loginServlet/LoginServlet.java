6a55eea410f5eaf71a17e7ae48f3a42b56f4a2bf
Gabriel Valente <112831085+ValentePG@users.noreply.github.com>
2024-11-12 17:57:42 -0300

    brincando de io

--- src/main/java/dev/valente/servelet/loginServlet/LoginServlet.java
+++ src/main/java/dev/valente/servelet/loginServlet/LoginServlet.java
@@ -17,11 +17,29 @@ public class LoginServlet extends HttpServlet {
 
         Gson gson = new Gson();
 
+//        InputStream inputStream = req.getInputStream();
+//        FileOutputStream arquivo = new FileOutputStream(caminhodearquivo);
+//
+//        byte[] bytes1 = new byte[1024];
+//
+//        while(inputStream.available()>0){
+//            inputStream.read(bytes1);
+//            arquivo.write(bytes1);
+//        }
+//        inputStream.close();
+//        arquivo.close();
 
-        // transforma o json em vetor de byte, não encontrei solução melhor, talvez seja uma instrução pesada
+        // InputStream inputStream = req.getInputStream();
+        // byte[] bytes = new byte[1024];
+        // StringBuilder stringBuilder = new StringBuilder();
+        // while(inputStream.available()>0){ inputStream.read(bytes);
+        // stringBuilder.append(bytes);
+        // }
+
+        // transforma o json em vetor de byte
         byte[] bytes = req.getInputStream().readAllBytes();
 
-        // Tenta transformar os bytes em String, não encontrei solução melhor, talvez seja uma instrução pesada
+        // Tenta transformar os bytes em String
         String msgDecode = new String(bytes);
 
         // Biblioteca GSON para transforamr a string "JSON" em classe java
